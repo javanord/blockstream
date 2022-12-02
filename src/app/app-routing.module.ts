7c52c2ed434238f837cdcfda1e03956aca00754c
mithun15 <mithun.kumarr6@gmail.com>
2022-12-02 10:00:23 +0000

    created tokenization and customer components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './components/customer/customer.component';
 import { LoginComponent } from './components/login/login.component';
+import { TokenizationComponent } from './components/tokenization/tokenization.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/login',pathMatch:'full'},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'customer', component: CustomerComponent ,children: [
+    {path: '', redirectTo: 'tokenization', pathMatch: 'full'},
+    {path: 'tokenization', component: TokenizationComponent}
+  ]},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from './shared/shared.module';
+import { TokenizationComponent } from './components/tokenization/tokenization.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    TokenizationComponent,
+    CustomerComponent,
   ],
   imports: [
     BrowserModule,

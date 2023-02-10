1df8f1cb81469c8fe2425972a02cbe93d92613cc
Subhash Kocher <subhash.kocher@ihsmarkit.com>
2023-02-10 18:30:18 +0530

    added status tradedate and failure reason

--- src/app/components/smart-trade/smart-trade.component.ts
+++ src/app/components/smart-trade/smart-trade.component.ts
@@ -2,19 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CustomerService } from 'src/app/services/customer.service';
 import { Currency } from 'src/models/currency.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-smart-trade',
   templateUrl: './smart-trade.component.html',
   styleUrls: ['./smart-trade.component.css'],
+  providers: [DatePipe]
 })
 export class SmartTradeComponent implements OnInit {
   usersList!: any[];
   currenciesList!: Currency[];
   smartTradeForm!: FormGroup;
   directionList !: [{direction : 'BUY'}, {direction: 'SELL'}];
-
-  constructor(private customerService: CustomerService) {
+  
+  constructor(private customerService: CustomerService, private datePipe: DatePipe) {
+    
     this.smartTradeForm = new FormGroup({
       counterParty: new FormControl(null),
       currencyBuy: new FormControl(0),
@@ -29,6 +32,7 @@ export class SmartTradeComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    
     this.customerService.getUsers().subscribe((res: any) => {
       this.usersList = res;
     });
@@ -40,8 +44,9 @@ export class SmartTradeComponent implements OnInit {
   submitSmartTrade() {
     const formData = this.smartTradeForm.value;
     const userHash = localStorage.getItem('userHash');
+    const curtDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd'); 
     this.customerService
-      .submitSmartTrade({ ...formData, tradingParty: userHash })
+      .submitSmartTrade({ ...formData, tradingParty: userHash, tradeDate: curtDate, status: 'Submitted' })
       .subscribe((res) => {
         alert('Smart trade submitted successfully');
         this.smartTradeForm.patchValue({
